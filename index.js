a5b54aa6d6398aed152979d7ba4df5733ce4f10d
Mike Fogel <mike@fogel.ca>
2020-12-03 10:00:18 -0300

    chore: basic implementation

---
+++ index.js
@@ -0,0 +1,49 @@
+const {DynamoDBClient, GetItemCommand} = require('@aws-sdk/client-dynamodb')
+const {strict: assert} = require('assert')
+
+exports.register = function () {
+  this.client = new DynamoDBClient()
+  this.tableName = this.config.get('rcpt-dynamo.table-name')
+  this.hashKeyName = this.config.get('rcpt-dynamo.hash-key-name')
+  assert(this.tableName, 'rcpt-dynamo.table-name must be configured')
+  assert(this.hashKeyName, 'rcpt-dynamo.hash-key-name must be configured')
+  this.register_hook('rcpt', 'rcpt')
+}
+
+exports.rcpt = function (next, connection, params) {
+  // Not sure why we need to do this check, but seems the default plugins do it so we will too
+  // https://github.com/haraka/Haraka/blob/v2.8.19/plugins/rcpt_to.in_host_list.js#L22-L23
+  // https://github.com/haraka/haraka-plugin-rcpt-ldap/blob/7f51384aa/index.js#L33-L34
+  const txn = connection.transaction
+  if (!txn) return
+
+  const address = params[0].address().toLowerCase()
+  connection.logdebug(this, `Checking dynamo for '${address}'`)
+
+  this.client
+    .send(
+      new GetItemCommand({
+        TableName: this.tableName,
+        Key: {[this.hashKeyName]: {S: address}},
+        ProjectionExpression: this.hashKeyName,
+      }),
+    )
+    .then(
+      ({Item}) => {
+        if (Item) {
+          connection.loginfo(this, `Accepting rcpt found in dynamo: '${address}'`)
+          txn.results.add(this, {pass: 'address-found'})
+          return next(OK)
+        } else {
+          connection.loginfo(this, `Rejecting rcpt not found in dynamo: '${address}'`)
+          txn.results.add(this, {fail: 'address-not-found'})
+          return next()
+        }
+      },
+      (err) => {
+        connection.logerror(this, `Rejecting rctp '${address}', dynamo query error: '${err}'`)
+        txn.results.add(this, {err: err})
+        return next()
+      },
+    )
+}
---
+++ index.spec.js
@@ -0,0 +1,175 @@
+/* eslint-env jest */
+jest.mock('@aws-sdk/client-dynamodb')
+const {DynamoDBClient, GetItemCommand} = require('@aws-sdk/client-dynamodb')
+const {rcpt, register} = require('.')
+const {AssertionError} = require('assert')
+
+const getRandomString = () => Math.random().toString(36).substring(4)
+
+describe('register()', () => {
+  const getThisMock = (configMock) => {
+    return {
+      config: {get: (key) => configMock[key]},
+      register_hook: jest.fn(),
+    }
+  }
+
+  it('throws if rcpt-dynamo.table-name is not set', () => {
+    const thisMock = getThisMock({'rcpt-dynamo.hash-key-name': getRandomString()})
+    expect(() => register.call(thisMock)).toThrow(AssertionError)
+    expect(() => register.call(thisMock)).toThrow(/rcpt-dynamo.table-name/)
+    expect(thisMock.register_hook).not.toHaveBeenCalled()
+  })
+
+  it('throws if rcpt-dynamo.hash-key-name is not set', () => {
+    const thisMock = getThisMock({'rcpt-dynamo.table-name': getRandomString()})
+    expect(() => register.call(thisMock)).toThrow(AssertionError)
+    expect(() => register.call(thisMock)).toThrow(/rcpt-dynamo.hash-key-name/)
+    expect(thisMock.register_hook).not.toHaveBeenCalled()
+  })
+
+  it('sets rcpt-dynamo.table-name and rcpt-dynamo.hash-key-name to config object', () => {
+    const tableName = getRandomString()
+    const hashKeyName = getRandomString()
+    const thisMock = getThisMock({
+      'rcpt-dynamo.table-name': tableName,
+      'rcpt-dynamo.hash-key-name': hashKeyName,
+    })
+    register.call(thisMock)
+    expect(thisMock.tableName).toBe(tableName)
+    expect(thisMock.hashKeyName).toBe(hashKeyName)
+    expect(thisMock.register_hook).toHaveBeenCalled()
+  })
+
+  it('initializes the aws dynamo client', () => {
+    const thisMock = getThisMock({
+      'rcpt-dynamo.table-name': getRandomString(),
+      'rcpt-dynamo.hash-key-name': getRandomString(),
+    })
+    const client = {}
+    DynamoDBClient.mockReturnValue(client)
+    register.call(thisMock)
+    expect(DynamoDBClient).toHaveBeenCalledTimes(1)
+    expect(DynamoDBClient).toHaveBeenCalledWith()
+    expect(thisMock.client).toBe(client)
+  })
+
+  it('registers rcpt hook correctly', () => {
+    const thisMock = getThisMock({
+      'rcpt-dynamo.table-name': getRandomString(),
+      'rcpt-dynamo.hash-key-name': getRandomString(),
+    })
+    register.call(thisMock)
+    expect(thisMock.register_hook).toHaveBeenCalledTimes(1)
+    expect(thisMock.register_hook).toHaveBeenCalledWith('rcpt', 'rcpt')
+  })
+})
+
+describe('rcpt()', () => {
+  const next = jest.fn()
+  const connection = {
+    logdebug: jest.fn(),
+    loginfo: jest.fn(),
+    logerror: jest.fn(),
+    transaction: {
+      results: {
+        add: jest.fn(),
+      },
+    },
+  }
+  let address = null
+  let params = null
+
+  beforeEach(() => {
+    address = getRandomString()
+    params = [{address: () => address}]
+  })
+
+  it('returns immediately if the connection has no transaction', () => {
+    const thisMock = {client: new DynamoDBClient()}
+    const {transaction, ...connectionNoTxn} = connection
+    expect(rcpt.call(thisMock, next, connectionNoTxn, params)).toBeUndefined()
+    expect(next).not.toHaveBeenCalled()
+    expect(connection.logdebug).not.toHaveBeenCalled()
+    expect(thisMock.client.send).not.toHaveBeenCalled()
+  })
+
+  it('sends the correct query to dynamo', async () => {
+    // configure
+    const thisMock = {client: new DynamoDBClient()}
+    const cmd = {}
+    GetItemCommand.mockReturnValue(cmd)
+    thisMock.client.send.mockResolvedValue({})
+    thisMock.tableName = getRandomString()
+    thisMock.hashKeyName = getRandomString()
+    // execute
+    await rcpt.call(thisMock, next, connection, params)
+    // verify
+    expect(GetItemCommand).toHaveBeenCalledTimes(1)
+    expect(GetItemCommand).toHaveBeenCalledWith({
+      TableName: thisMock.tableName,
+      Key: {[thisMock.hashKeyName]: {S: address}},
+      ProjectionExpression: thisMock.hashKeyName,
+    })
+    expect(thisMock.client.send).toHaveBeenCalledTimes(1)
+    expect(thisMock.client.send.mock.calls[0]).toHaveLength(1)
+    expect(thisMock.client.send.mock.calls[0][0]).toBe(cmd)
+  })
+
+  it('accepts an address found in dynamo', async () => {
+    // configure
+    global.OK = {}
+    const thisMock = {client: new DynamoDBClient()}
+    thisMock.client.send.mockResolvedValue({Item: true})
+    // execute
+    await rcpt.call(thisMock, next, connection, params)
+    // verify
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(next).toHaveBeenCalledWith(global.OK)
+    expect(connection.loginfo).toHaveBeenLastCalledWith(
+      thisMock,
+      `Accepting rcpt found in dynamo: '${address}'`,
+    )
+    expect(connection.transaction.results.add).toHaveBeenCalledTimes(1)
+    expect(connection.transaction.results.add).toHaveBeenCalledWith(thisMock, {
+      pass: 'address-found',
+    })
+  })
+
+  it('rejects an address not found in dynamo', async () => {
+    // configure
+    const thisMock = {client: new DynamoDBClient()}
+    thisMock.client.send.mockResolvedValue({})
+    // execute
+    await rcpt.call(thisMock, next, connection, params)
+    // verify
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(next).toHaveBeenCalledWith()
+    expect(connection.loginfo).toHaveBeenLastCalledWith(
+      thisMock,
+      `Rejecting rcpt not found in dynamo: '${address}'`,
+    )
+    expect(connection.transaction.results.add).toHaveBeenCalledTimes(1)
+    expect(connection.transaction.results.add).toHaveBeenCalledWith(thisMock, {
+      fail: 'address-not-found',
+    })
+  })
+
+  it('rejects address and records error if dynamo throws an error', async () => {
+    // configure
+    const thisMock = {client: new DynamoDBClient()}
+    const err = new Error(getRandomString())
+    thisMock.client.send.mockRejectedValue(err)
+    // execute
+    await rcpt.call(thisMock, next, connection, params)
+    // verify
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(next).toHaveBeenCalledWith()
+    expect(connection.logerror).toHaveBeenLastCalledWith(
+      thisMock,
+      `Rejecting rctp '${address}', dynamo query error: '${err}'`,
+    )
+    expect(connection.transaction.results.add).toHaveBeenCalledTimes(1)
+    expect(connection.transaction.results.add).toHaveBeenCalledWith(thisMock, {err: err})
+  })
+})
